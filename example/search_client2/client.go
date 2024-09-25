5a1a0d54fd70f8b18a7766616ba3d857d6d93b54
vearne <asdwoshiaotian@gmail.com>
2024-09-25 11:46:08 +0800

    update example

---
+++ example/search_client2/client.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/robfig/cron/v3"
+	pb "github.com/vearne/grpcreplay/example/service_proto"
+	"github.com/vearne/grpcreplay/example/service_proto/another"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	_ "google.golang.org/grpc/encoding/gzip" // Registration of gzip Compressor will be completed
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+const address = "127.0.0.1:35001"
+
+func main() {
+	// Seconds field, required
+	c := cron.New(cron.WithSeconds())
+	spec := "*/3 * * * * *"
+	c.AddFunc(spec, execute)
+	c.Run()
+}
+
+func execute() {
+	conn, err := grpc.Dial(address,
+		grpc.WithInsecure(),
+		//grpc.WithDefaultCallOptions(grpc.UseCompressor("gzip")),
+	)
+	if err != nil {
+		log.Fatalf("grpc.Dial err: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewSearchServiceClient(conn)
+	counter := 0
+	timeCounter := 0
+	muchCounter := 0
+	for i := 0; i < 1000; i++ {
+		value := rand.Intn(100)
+		if value <= 5 {
+			counter++
+			sendSearch(client, counter)
+		} else if value <= 20 {
+			muchCounter++
+			sendMuch(client, muchCounter)
+		} else {
+			timeCounter++
+			sendCurrTime(client, timeCounter)
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func sendSearch(client pb.SearchServiceClient, i int) {
+	// add some headers
+	md := metadata.New(map[string]string{
+		"testkey1": "testvalue1",
+		"testkey2": "testvalue2",
+	})
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+
+	resp, err := client.Search(ctx,
+		&pb.SearchRequest{
+			StaffName: "zhangsan",
+			Age:       uint32(i),
+			Gender:    true,
+			Extra: &pb.ExtraInfo{
+				JobTitle:   "software engineer",
+				Location:   "Beijing",
+				Department: &another.Department{Id: 2000, Name: "backend"},
+			},
+		},
+	)
+	if err != nil {
+		statusErr, ok := status.FromError(err)
+		if ok {
+			if statusErr.Code() == codes.DeadlineExceeded {
+				log.Fatalln("client.Search err: deadline")
+			}
+		}
+
+		log.Fatalf("client.Search err: %v", err)
+	}
+
+	bt, _ := json.Marshal(resp)
+	log.Println("resp:", string(bt))
+}
+
+func sendCurrTime(client pb.SearchServiceClient, id int) {
+	md := metadata.New(map[string]string{
+		"testkey3": "testvalue3",
+		"testkey4": "testvalue4",
+	})
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	resp, err := client.CurrentTime(
+		ctx,
+		&pb.TimeRequest{RequestId: uint64(id)},
+	)
+	if err != nil {
+		log.Fatalf("client.CurrentTime err: %v", err)
+	}
+	bt, _ := json.Marshal(resp)
+	log.Println("resp:", string(bt))
+}
+
+func sendMuch(client pb.SearchServiceClient, id int) {
+	md := metadata.New(map[string]string{
+		"testkey5": "testvalue5",
+		"testkey6": "testvalue6",
+	})
+	req := &pb.MuchRequest{RequestId: uint64(id)}
+	req.Books = make([]*pb.Book, 1000)
+	for i := 0; i < 1000; i++ {
+		req.Books[i] = &pb.Book{Name: "name" + strconv.Itoa(id*1000+i), Id: int32(id)}
+	}
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	resp, err := client.SendMuchData(
+		ctx,
+		req,
+	)
+	if err != nil {
+		log.Fatalf("client.SendMuchData err: %v", err)
+	}
+
+	log.Println("id", id, resp.RequestId)
+	bt, _ := json.Marshal(resp)
+	log.Println("resp:", string(bt))
+}
--- go.mod
+++ go.mod
@@ -38,6 +38,7 @@ require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/sirupsen/logrus v1.4.2 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/tidwall/gjson v1.13.0 // indirect
